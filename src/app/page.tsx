3994b05238518523d5b55084ba757567be31e03b
Akshataubhale <akshatagithub@gmail.com>
2025-08-17 21:14:59 +0530

    02:database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
-import {
-  Accordion,
-  AccordionItem,
-  AccordionTrigger,
-  AccordionContent
-} from '@radix-ui/react-accordion'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-<Accordion type="single" collapsible>
-  <AccordionItem value="item-1">
-    <AccordionTrigger>Title</AccordionTrigger>
-    <AccordionContent>Content</AccordionContent>
-  </AccordionItem>
-</Accordion>
+      hello
     </div>
   )
 }
